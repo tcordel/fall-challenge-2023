04272bbeded418e1ec6c04d4b790185e5af1b3c3
Thibault <tcordel@enovacom.fr>
2024-01-06 14:04:19 +0100

    Fixing attack strategy

--- src/main/java/fr/tcordel/model/AttackFish.java
+++ src/main/java/fr/tcordel/model/AttackFish.java
@@ -8,14 +8,28 @@ public class AttackFish extends AbstractStrat {
 	Vector process(Fish fish, Drone drone) {
 		Vector target = null;
 		Vector pos = null;
-		boolean visible = false;
-		if (fish.pos != null) {
-			visible = true;
-			//			game.updateSingleFleeingFish(fish, drone);
+		boolean visible = fish.pos != null;
+		if (visible) {
 			pos = game.snapToFishZone(fish.pos.add(fish.speed), fish);
-			int offset = ((pos.getX() < (Game.WIDTH / 2)) ?  1 : -1) * Game.DARK_SCAN_RANGE;
-			target = new Vector((int)pos.getX() + offset - drone.getX(),
-				(int)pos.getY() - drone.getY());
+			int offset = ((pos.getX() < (Game.WIDTH / 2)) ? 1 : -1) * Game.DARK_SCAN_RANGE;
+			double targetX = fish.pos.getX() + offset;
+			double deltaTargetX = targetX - drone.getX();
+			double deltaTargetY = pos.getY() - drone.getY();
+			if (fish.pos.distance(drone.pos) >= Game.FISH_HEARING_RANGE) {
+
+				target = new Vector((int)pos.getX() + offset - drone.getX(),
+					deltaTargetY);
+			} else if (Math.abs(deltaTargetX) >= 450) {
+				System.err.println("Correcting X for drone " + drone.id + "to attack " + fish.id);
+				target = new Vector(deltaTargetX, 0);
+			} else if (Math.abs(deltaTargetY) >= 450) {
+				System.err.println("Correcting Y for drone " + drone.id + "to attack " + fish.id);
+				target = new Vector(0,
+					deltaTargetY);
+			} else {
+				target = new Vector((int)pos.getX() + offset - drone.getX(),
+					deltaTargetY);
+			}
 		} else {
 			target = drone.getRadar().get(fish.id)
 				.getDirection();
